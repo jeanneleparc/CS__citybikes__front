12885d281440807320421c805875f37983dcc8bb
Bouyssel Camille <camille.bouyssel@student-cs.fr>
2021-12-01 14:57:04 +0100

    data service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-front';
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+
+    this.dataService.sendGetRequest().subscribe((data)=>{
+      console.log(data);
+    })  
+  }
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private REST_API_SERVER = "http://localhost:8000/station_status";
+
+  constructor(private httpClient: HttpClient) { }
+
+  public sendGetRequest(){
+    return this.httpClient.get(this.REST_API_SERVER);
+  }
+}
