a518a13474fbe9993818f72f2610d6071f590d40
BOUYSSEL Camille <45000497+BouysselCamille@users.noreply.github.com>
2021-12-01 16:53:03 +0100

    Display station status (#1)
    * update readme
    
    * add banner and footer
    
    * add icon and bootstrap
    
    * data service
    
    * log data
    
    * display data
    
    * name of tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-front';
+  data: any = {};
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+
+    this.dataService.sendGetRequest().subscribe((data)=>{
+      this.data = data[0];
+    })  
+  }
 }
