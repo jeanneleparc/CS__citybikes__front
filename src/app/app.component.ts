9b554bcef36669dcbff6a1218d4458364fa5390e
jeanneleparc <jeanne.leparc@wanadoo.fr>
2022-01-06 16:39:26 +0100

    Premier essai avec une map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { DataService } from './data.service';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  private map : any;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+
+  const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    minZoom: 3,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  });
+
+  tiles.addTo(this.map);
+}
+
   data: any = {};
   constructor(private dataService: DataService) {}
 
@@ -16,4 +34,18 @@ export class AppComponent {
       this.data = data;
     });
   }
+
+  ngAfterViewInit(): void {
+    this.map = L.map('map').setView([ 40.77, -73.968565], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: ''
+    }).addTo(this.map);
+    //this.initMap();
+  }
+
 }
