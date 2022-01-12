9dbc50620fead3ca649824e3645332ab1fc34a08
Bouyssel Camille <camille.bouyssel@student-cs.fr>
2022-01-12 17:11:39 +0100

    remove karma eslint ignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [AppComponent],
