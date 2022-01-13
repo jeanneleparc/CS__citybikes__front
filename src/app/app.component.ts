f62723119d2cfb5ae1c5b8ebbd3be38c6640afa3
clarani <clara.ni@student-cs.fr>
2022-01-13 10:13:49 +0100

    Ajout des markers et du sidepannel + clean du code front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,44 +2,49 @@ import { Component, AfterViewInit } from '@angular/core';
 import { DataService } from './data.service';
 import * as L from 'leaflet';
 
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements AfterViewInit {
+  stations: any = {};
+  selectedStation: any = {};
   private map : any;
-  public last_updated_time_string : string = '';
+  sidebar: any;
+  sidebarIsVisible: boolean = false;
+  lastUpdatedTime : string = '';
 
-  private initMap(): void {
-    this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
-    });
-
-  const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
-    minZoom: 3,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  });
-
-  tiles.addTo(this.map);
-}
-
-  data: any = {};
-  constructor(private dataService: DataService) {}
-
-  ngOnInit() {
-    this.dataService.sendGetRequest().subscribe((data) => {
-      console.log('Number of stations displayed : ', data.length);
-      this.data = data;
-      const last_updated_time = new Date(data[0].last_updated)
-      this.last_updated_time_string = `${last_updated_time.toLocaleTimeString()} on ${last_updated_time.toDateString()}`
-      console.log(last_updated_time.toLocaleTimeString(), 'on', last_updated_time.toDateString());
-    })  
-  }
+  constructor(private dataService: DataService) { }
 
   ngAfterViewInit(): void {
+    this.initMap();
+
+    this.dataService.sendGetRequest().subscribe((data)=>{
+      console.log("Number of stations displayed : ", data.length);
+      this.stations = data;
+      console.log(data[0]);
+      this.setUpLastUpdatedTime(data[0].last_updated);
+      this.addMarkers();
+    })
+  }
+
+  private initMap(): void {
     this.map = L.map('map').setView([ 40.77, -73.968565], 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -49,7 +54,52 @@ export class AppComponent implements AfterViewInit {
       zoomOffset: -1,
       accessToken: 'pk.eyJ1IjoiY2xhcmEtbmkiLCJhIjoiY2t5MzQ3cXQ2MHJ5ZjJybWtmN2w5b3dqMSJ9.zrQ2bq62jaJdYXSPmvxMKA'
     }).addTo(this.map);
-    //this.initMap();
+
+    // sidebar
+    this.sidebar = document.getElementById("mySidepanel");
   }
 
+  addMarkers(): void {
+    for (const station of this.stations) {
+      const { latitude, longitude } = station;
+      const marker = L.marker([latitude, longitude]);
+      // marker.bindPopup(this.createMarkerPopup(station)); // to add popup
+      
+      marker.addTo(this.map).on('click',  () => {
+        console.log('On va toggle la sidebar !');
+        if (this.sidebarIsVisible) {
+          if (station != this.selectedStation) {
+            this.selectedStation = station
+          } else {
+            this.closeSidebar();
+          }
+        } else {
+          this.openSidebar(station);
+        }
+      });
+    }
+  }
+
+  createMarkerPopup(station: any): string {
+    return `` +
+      `<h6>Name: ${ station.name }</h6>` +
+      `<div>${ station.num_bikes_available } available bikes</div>` +
+      `<div>${ station.num_docks_available } available docks</div>`
+  }
+
+  setUpLastUpdatedTime(brutLastUpdatedTime: string) {
+    const lastUpdatedDate = new Date(brutLastUpdatedTime)
+    this.lastUpdatedTime = `${lastUpdatedDate.toLocaleTimeString()} on ${lastUpdatedDate.toDateString()}`
+  }
+
+  openSidebar(station: any) {
+    this.selectedStation = station;
+    this.sidebar.style.width = "250px";
+    this.sidebarIsVisible = true;
+  }
+
+  public closeSidebar() {
+    this.sidebar.style.width = "0";
+    this.sidebarIsVisible = false;
+  }
 }
