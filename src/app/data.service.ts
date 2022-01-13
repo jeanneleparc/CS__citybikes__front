d7cabf6f883dd85a0621bb28fe7eb57bde73e1d4
BOUYSSEL Camille <45000497+BouysselCamille@users.noreply.github.com>
2022-01-13 09:56:15 +0100

    add prettier (#8)
    * add prettier
    
    * remove karma eslint ignore
    
    * add prettier fix sc ript

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,16 +1,15 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  private REST_API_SERVER = 'http://localhost:8000/station_status';
 
-  private REST_API_SERVER = "http://localhost:8000/station_status";
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-
-  public sendGetRequest(){
+  public sendGetRequest() {
     return this.httpClient.get<any>(this.REST_API_SERVER);
   }
 }
