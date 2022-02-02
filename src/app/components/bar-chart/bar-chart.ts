84dc2d9d48424e7f92c2b2d31df8a404f196045b
jeanneleparc <jeanne.leparc@wanadoo.fr>
2022-02-02 16:36:42 +0100

    Ajout fleches navigation histo

--- src/app/components/bar-chart/bar-chart.ts
+++ src/app/components/bar-chart/bar-chart.ts
@@ -4,6 +4,12 @@ import { DataService } from 'src/app/data.service';
 import * as moment from 'moment-timezone';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
+import * as _ from 'lodash';
+
+export interface IStatsAvgFillingRate {
+  timeSlot: number;
+  avgFillingRate: number;
+}
 
 @Component({
   selector: 'bar-chart',
@@ -14,11 +20,37 @@ export class BarChart implements OnInit {
   @Input() $selectedStation: BehaviorSubject<any> = new BehaviorSubject({});
 
   idStation!: number;
+  date!: moment.Moment;
   day: string = '';
   hour!: number;
-  statsAvgFillingRate: any[] = [];
+  statsAvgFillingRate: IStatsAvgFillingRate[] = [];
 
-  public barChartOptions: ChartOptions = {};
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    scales: {
+      yAxes: [
+        {
+          display: true,
+          ticks: {
+            min: 0,
+            max: 100,
+            stepSize: 100,
+          },
+          gridLines: {
+            offsetGridLines: false,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          display: true,
+          ticks: {
+            maxTicksLimit: 5,
+          },
+        },
+      ],
+    },
+  };
   public barChartLabels: Label[] = [];
   public barChartType: ChartType = 'bar';
   public barChartLegend = false;
@@ -28,10 +60,11 @@ export class BarChart implements OnInit {
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.date = moment().tz('America/New_York');
     this.day = moment().tz('America/New_York').format('dddd');
     this.hour = parseInt(moment().tz('America/New_York').format('HH'), 10);
     this.$selectedStation.subscribe((selectedStation) => {
-      if (!selectedStation.id) {
+      if (_.isNil(selectedStation) || _.isEmpty(selectedStation)) {
         return;
       }
       this.idStation = selectedStation.id;
@@ -39,58 +72,27 @@ export class BarChart implements OnInit {
         .sendGetAvgFillingRateByIdByDayRequest(this.idStation, this.day)
         .subscribe((data) => {
           this.statsAvgFillingRate = data;
-          this.barChartLabels = data.map((element: any) =>
+          this.barChartLabels = data.map((element: IStatsAvgFillingRate) =>
             moment.utc(element.timeSlot * 3600 * 1000).format('hh a')
           );
-          const dataTest = data.map((element: any) =>
-            Math.round(element.avgFillingRate * 100)
+          const avgFillingRatePercent = data.map(
+            (element: IStatsAvgFillingRate) =>
+              Math.round(element.avgFillingRate * 100)
           );
-          this.barChartOptions = {
-            responsive: true,
-            scales: {
-              yAxes: [
-                {
-                  display: true,
-                  ticks: {
-                    min: 0,
-                    max: 100,
-                    stepSize: 100,
-                  },
-                  gridLines: {
-                    offsetGridLines: false,
-                  },
-                },
-              ],
-              xAxes: [
-                {
-                  display: true,
-                  ticks: {
-                    maxTicksLimit: 5,
-                  },
-                },
-              ],
-            },
-            title: {
-              text: this.day,
-              display: true,
-            },
-          };
           const backgroundColorTab: string[] = [];
           const borderColorTab: string[] = [];
-          data.forEach(
-            (element: { timeSlot: number; avgFillingRate: number }) => {
-              if (element.timeSlot !== this.hour) {
-                backgroundColorTab.push('#ffa05b');
-                borderColorTab.push('#ffa05b');
-                return;
-              }
-              backgroundColorTab.push('#ff6b00');
-              borderColorTab.push('#ff6b00');
+          data.forEach((element: IStatsAvgFillingRate) => {
+            if (element.timeSlot !== this.hour) {
+              backgroundColorTab.push('#ffa05b');
+              borderColorTab.push('#ffa05b');
+              return;
             }
-          );
+            backgroundColorTab.push('#ff6b00');
+            borderColorTab.push('#ff6b00');
+          });
           this.barChartData = [
             {
-              data: dataTest,
+              data: avgFillingRatePercent,
               label: 'Average Filling Rate',
               backgroundColor: backgroundColorTab,
               borderColor: borderColorTab,
@@ -100,5 +102,39 @@ export class BarChart implements OnInit {
           ];
         });
     });
+  }
+
+  goToPreviousDay(): void {
+    this.date = this.date.subtract(1, 'day');
+    this.day = this.date.clone().format('dddd');
+    this.hour = parseInt(this.date.clone().format('HH'), 10);
+    this.dataService
+      .sendGetAvgFillingRateByIdByDayRequest(this.idStation, this.day)
+      .subscribe((dataCall) => {
+        this.statsAvgFillingRate = dataCall;
+        const avgFillingRatePercent = dataCall.map(
+          (element: IStatsAvgFillingRate) =>
+            Math.round(element.avgFillingRate * 100)
+        );
+
+        this.barChartData[0].data = avgFillingRatePercent;
+      });
+  }
+
+  goToNextDay(): void {
+    this.date = this.date.add(1, 'day');
+    this.day = this.date.clone().format('dddd');
+    this.hour = parseInt(this.date.clone().format('HH'), 10);
+    this.dataService
+      .sendGetAvgFillingRateByIdByDayRequest(this.idStation, this.day)
+      .subscribe((dataCall) => {
+        this.statsAvgFillingRate = dataCall;
+        const avgFillingRatePercent = dataCall.map(
+          (element: IStatsAvgFillingRate) =>
+            Math.round(element.avgFillingRate * 100)
+        );
+
+        this.barChartData[0].data = avgFillingRatePercent;
+      });
   }
 }
